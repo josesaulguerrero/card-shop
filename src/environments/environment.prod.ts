9f6357914d4461e600fa9b7a12dd308e7ce5d22c
Jose Guerrero Serrano <jose.guerrero@sofka.com.co>
2023-02-07 09:03:19 -0500

    Fix: firebase project config was wrong.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
 	firebase: {
-		projectId: 'card-shop-v2',
-		appId: '1:651215758956:web:93e0cb617bbf0e2cd51a49',
-		storageBucket: 'card-shop-v2.appspot.com',
-		apiKey: 'AIzaSyD0dqok0xye8BTOghVW-18U4TxdnD9BT2c',
-		authDomain: 'card-shop-v2.firebaseapp.com',
-		messagingSenderId: '651215758956',
+		apiKey: 'AIzaSyDLIFXP9ydHaBBfXv5pEG9bQ4EqycxbFeg',
+		authDomain: 'card-shop-a7bf4.firebaseapp.com',
+		projectId: 'card-shop-a7bf4',
+		storageBucket: 'card-shop-a7bf4.appspot.com',
+		messagingSenderId: '987158835284',
+		appId: '1:987158835284:web:2a50fcd6d81bd7b7e5fdc1',
 	},
 	production: true,
 };
